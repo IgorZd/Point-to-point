d93d9805274c36d5c2b30f655f667d7b6ff032bb
IgorZd <igor.zd.43@gmail.com>
2022-11-17 09:39:23 +0200

    fix: serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./app/store";
+import * as serviceWorker from "./serviceWorker";
 import "./styles/global.css";
-import reportWebVitals from "./reportWebVitals";
 import { StyleContextProvider } from "./styles/style.context";
 import { routes } from "./routes";
 import { renderRoutes } from "react-router-config";
@@ -22,7 +22,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
